646154eb5881a8f2848b8b2cd6890926379fa823
raf-zig <ziravin@yandex.ru>
2023-06-16 20:22:26 +0500

    Add associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
+  belongs_to :user
   validates :title, presence: true, uniqueness: true
   validates :body, presence: true, length: {minimum: 3}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :posts
+
   validates :username, presence: true, length: {in: 3..15}, uniqueness: true
   validates :email, presence: true, uniqueness: true
 end
