83c8da2376c88a3eaf3142daa650b5ee3ba67874
raf-zig <ziravin@yandex.ru>
2023-06-16 22:24:04 +0500

    Add commit model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :posts
-
+  has_many :comments
   validates :username, presence: true, length: {in: 3..15}, uniqueness: true
   validates :email, presence: true, uniqueness: true
 end
